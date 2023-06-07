a5f8ec2903b57f36d18732a48c6f17f9ab223dd7
barbazyaka5 <108088162+barbazyaka5@users.noreply.github.com>
2023-06-07 15:56:18 +0300

    Add files via upload

---
+++ last/sessia3/sessia3/MainWindow.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace sessia3
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            frameOut.Navigate(new outFlighs());
+            oneBox.ItemsSource = Session3_XXEntities.GetContext().Countries.ToList();
+            twoBox.ItemsSource = Session3_XXEntities.GetContext().Countries.ToList();
+            threeBox.ItemsSource = Session3_XXEntities.GetContext().CabinTypes.ToList();
+            frameToo.Navigate(new tooPage());
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("К сожалению рейсов по вашему запросу нет");
+        }
+
+        private void new_Click(object sender, RoutedEventArgs e)
+        {
+            twoo tr = new twoo();
+            tr.Show();
+            Close();
+        }
+    }
+}
---
+++ last/sessia3/sessia3/twoo.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace sessia3
+{
+    /// <summary>
+    /// Логика взаимодействия для twoo.xaml
+    /// </summary>
+    public partial class twoo : Window
+    {
+        public twoo()
+        {
+            InitializeComponent();
+            frameOut.Navigate(new addPass());
+            frameToo.Navigate(new passen());
+        }
+
+        private void bck_Click(object sender, RoutedEventArgs e)
+        {
+            MainWindow main = new MainWindow();
+            main.Show();
+            Close();
+        }
+
+        private void new_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Новый пассажир добавлен!");
+        }
+    }
+}
